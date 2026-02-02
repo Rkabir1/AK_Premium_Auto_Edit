069f9faaff0880f785fb72992de9d0936714d099
Rkabir1 <133803632+Rkabir1@users.noreply.github.com>
2026-02-02 23:24:27 +0600

    lib/services/github_trigger.dart

--- lib/services/github_trigger.dart
+++ lib/services/github_trigger.dart
@@ -5,7 +5,8 @@ class GitHubTrigger {
   static Future<void> runWorkflow(
       String token, Map<String, dynamic> config) async {
     final url = Uri.parse(
-        "https://api.github.com/repos/YOURNAME/AK_Premium_Auto_Edit/dispatches");
+      "https://api.github.com/repos/YOUR_USERNAME/AK_Premium_Auto_Edit/dispatches",
+    );
 
     await http.post(
       url,
